bf0b394af5e03879310d8dd4f71057c599547ea1
Jordan <jordan.casadessus@gmail.com>
2024-02-27 02:56:18 +0100

    added group sh

--- setup.sh
+++ setup.sh
@@ -11,14 +11,12 @@ if  [ -d "/home/$USER/data/wordpress" ]; then \
 	mkdir /home/$USER/data/wordpress; \
 fi
 
-
+sudo groupadd docker
+sudo usermod -aG docker ${USER}
 sudo apt-get update
-sudo apt-get upgrade -y
 sudo apt-get install make curl lsb-release ca-certificates apt-transport-https software-properties-common hostsed -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update
 sudo apt-get install docker-ce -y
-sudo apt-get update
 sudo apt-get install docker-compose docker-compose-plugin -y
-sudo apt-get update
+sudo apt-get upgrade -y
